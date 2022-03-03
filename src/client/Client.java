4719c3d139ba06de8ed6d4bf3ca84bd36a2d5922
Shushan <shmatevosyatl@gmail.com>
2022-03-03 12:13:00 +0400

    new changes

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,31 +2,26 @@ package client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.net.Socket;
 
 
 public class Client {
 
-    private Socket clientSocket;
-    private DataOutputStream dos;
-    private DataInputStream dis;
-
     public Client(String ip, int port) throws Exception {
-        clientSocket = new Socket(ip, port);
+        Socket clientSocket = new Socket(ip, port);
 
         System.out.println("Welcome to Redis store.");
         System.out.println("Connected to server " + clientSocket.getRemoteSocketAddress());
         System.out.println("Write your Redis commands...");
 
-        dis = new DataInputStream(System.in);
-        dos = new DataOutputStream(clientSocket.getOutputStream());
+        DataInputStream dis = new DataInputStream(System.in);
+        DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
 
         String command = "";
 
         while (!command.equalsIgnoreCase("exit")) {
-            command = this.dis.readLine();
-            this.dos.writeUTF(command);
+            command = dis.readLine();
+            dos.writeUTF(command);
         }
 
         dis.close();
@@ -34,6 +29,8 @@ public class Client {
         clientSocket.close();
     }
 
-    public static void main(String[] args) throws Exception { new Client("127.0.0.1", 5000);}
+    public static void main(String[] args) throws Exception {
+        new Client("127.0.0.1", 5000);
+    }
 }
 
